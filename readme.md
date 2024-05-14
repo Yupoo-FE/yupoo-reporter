6ed4e8c5942ffa1615db41f9fe1d12c49d1a09fb
thinkasany <480968828@qq.com>
2024-05-14 19:46:36 +0800

    feat: support debug mode

--- readme.md
+++ readme.md
@@ -1,4 +1,15 @@
 # 使用指南
+
+## 引用
+
+cjs
+```js
+ const reportRequest = require('yupoo-reporter').default;
+```
+esm
+```
+import reportRequest from 'yupoo-reporter';
+```
 ```js
   const reportObj = {
     report_source: 'website',
@@ -10,6 +21,23 @@
   reportRequest(url, reportObj)
 ```
 
+# 类型
+```
+type ReportRequestParams = {
+    url: string;
+    reportObj: any;
+    debug?: boolean | string; // boolean 或者 自定义log前缀
+};
+```
+
+# debug 模式：
+```
+reportRequest(url, reportObj, true);
+
+or
+
+reportRequest(url, reportObj, 'debugger: ');
+```
 
 
 # 依赖注意事项
@@ -20,7 +48,7 @@
 
 master 分支为最新代码，为了不更新导致其他项目的崩溃，分出几个分支。如 1.1.x stable / impress-website(项目名) 单独去维护，有的项目不需要同步到最新的repoter sdk。
 
-# 好处
+# 优势
 1. 不用在项目中加一大堆兼容的代码, 在sdk中就提供了最向下兼容的产物。
 ```
 require('es6-promise').polyfill();
@@ -29,3 +57,11 @@ const reqwest = require('reqwest');
 ```
 2. 使用 tsup 打包兼容 esm / cjs 的sdk，不用在一套代码里封装两遍耦合度极高的utils。
 3. 后续图管所有上报都可以直接用这个通用的版本，降低切换项目上报的心智负担。
+
+# 开发
+
+开发可以不急着发npm，好好调试一下dist文件
+
+```
+const reportRequest = require('./dist').default;
+```
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import fetch from 'node-fetch'
 
-async function reportRequest (url: string, reportObj: any) {
+type ReportRequestParams = {
+    url: string;
+    reportObj: any;
+    debug?: boolean | string; // boolean 或者 自定义log前缀
+};
+
+async function reportRequest (url: ReportRequestParams['url'], reportObj: ReportRequestParams['reportObj'], debug: ReportRequestParams['debug'] = false)  {
     if (typeof window === 'object') {
         if (window?.navigator?.sendBeacon) {
-            return window.navigator.sendBeacon(`${window.location.origin}/r/c/a?name=yupoo_internal`, JSON.stringify(reportObj));
+            return window.navigator.sendBeacon(url, JSON.stringify(reportObj));
         }
     }
     return fetch(url, {
         method: "POST",
         body: JSON.stringify(reportObj)
-    }).then(() => {
+    }).then((res) => {
+        if (debug) {
+           if (typeof debug === 'boolean') {
+            console.log('debug:', res);
+           } else {
+            console.log(debug, res);
+           }
+        } 
     }).catch(err => {
       console.log('[report error]', err);
     });
